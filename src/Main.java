1cb5519055ff1e982a63f2c51a89cdcc45dc4f3f
Ram <bhujelti@mail.uc.edu>
2023-02-03 09:49:06 -0500

    Extra Credit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int evenNumber = 12;
+        int oddNumber = 7;
+
+        System.out.println( "The number " + evenNumber + " is an Even Number ");
+        System.out.println( "The number " + oddNumber + "is an Odd Number");
+
+        
+
+    }
+}
